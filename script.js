0e80a51b108c327be3739f63cbb6c0b98b76bf03
Jatin17248 <jatinsood242@gmail.com>
2024-07-15 01:01:28 +0530

    added song play functionality

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let currentSong = new Audio();
+
 async function getSongs() {
   let a = await fetch("http://omwebsolutions.000webhostapp.com/spotify/songs/");
   let response = await a.text();
@@ -12,29 +14,25 @@ async function getSongs() {
   const newSongs = songs.map((song) =>
     song.replace(
       "http://127.0.0.1:5500/",
-      "http://omwebsolutions.000webhostapp.com/spotify/songs/"
+      "https://omwebsolutions.000webhostapp.com/spotify/songs/"
     )
   );
   return newSongs;
 }
-
-async function main() {
-  let songs = await getSongs();
-  console.log(songs);
-  let songUl = document.querySelector(".songList ol");
+async function createSongList(songs, songUl){
   for (let song of songs) {
+    let mainUrl = song
     let li = document.createElement("li");
     song = song
       .split("/songs/")[1]
       .replaceAll("%20", " ")
       .replace(".mp3", "")
       .replace("(DJJOhAL.Com)", "");
-
     let songName = song.split("-")[0];
     let singerName = song.split("-")[1];
     li.innerHTML = `<i class="fa-solid fa-music"></i>
               <div class="info">
-                <div>${songName}</div>
+                <div class="songName" name="${mainUrl}">${songName}</div>
                 <div>${
                   singerName !== undefined ? singerName : "By Unknown"
                 }</div>
@@ -42,12 +40,38 @@ async function main() {
               <div class="play"><i class="fa-solid fa-play"></i></div>`;
     songUl.appendChild(li);
   }
+}
+
+const playMusic = (songUrl) => {
+  currentSong.src = songUrl;
+  currentSong.play();
+}
+
+async function main() {
+  let currentSong;
+  let songs = await getSongs();
+  //console.log(songs);
+  let songUl = document.querySelector(".songList ol");
+  await createSongList(songs, songUl);
+
+  Array.from(songUl.getElementsByTagName("li")).forEach((e) => {
+    const songElement = e.querySelector(".songName");
+    if (songElement) {
+      let songUrl = songElement.getAttribute("name");
+      songElement.parentElement.parentElement.addEventListener("click", () => {
+        playMusic(songUrl);
+      });
+    }
+  });
+  
   var audio = new Audio(songs[0]);
   //audio.play();
 
   audio.addEventListener("loadeddata", () => {
     console.log(audio.duration, audio.currentSrc, audio.currentTime);
   });
+
+  //Attatch an event listener to each song
 }
 
 main();

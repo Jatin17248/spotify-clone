52acc273873b9a5b5cdb67fb8b45df01dd8ce45d
Jatin17248 <jatinsood242@gmail.com>
2024-07-15 16:44:28 +0530

    added a file to server for removal of cors error

---
+++ proxy.php
@@ -0,0 +1,13 @@
+<?php
+// Allow access from any origin
+header("Access-Control-Allow-Origin: *");
+
+// URL of the original songs directory
+$songsUrl = 'https://test.brightjuniors.in/songs/';
+
+// Fetch the content from the original URL
+$response = file_get_contents($songsUrl);
+
+// Output the response
+echo $response;
+?>
